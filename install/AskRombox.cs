bc33e794c44a8a70dadbc68d8a1d8d75e0f0e619
X(T)🚒🚒🚒🚒🚒🚒AAAAAAAAAAAATTTTTTTTTTTTTTTTTTTTTTTTTTTT🚒🚒🚒TTTTTTTTTTTTTTTTTTTTTTTTTTTT'TT'|TTTTITX'{TT:}[''':;"'{'}['\:"|"'{'}[':]:'[':]:IMX'{:}['''X:;"'{'}['\:"|"'{'}[':]:I'{' '}['''X:;"'{'}['\:"|"'{'}[':]:'[':]:IꞮT'{;}['''X:;"'{'}['\:"|"'{}🚒🚒🚒🚒|🦍NX <shauntmcgovern@gmail.com>
2025-05-02 15:19:30 -0500

    LL'
    LL'(very complicated multi-use double)):09sx:/y;

--- install/AskRombox.cs
+++ install/AskRombox.cs
@@ -7,29 +7,36 @@ public AskRombox()
 	Found Purpose ()2ae'):-;00/')2({2')}0.A9'2A9?/_::'2/000.P')'A{M}IOP;)'{--}7s'+/='--__'Q'7s'7s?/2+/='{P}IOP;
 
 
-	double Tom/Roy = 8990.o.p;
+	double Tom/(//)'Roy = 8990.o.p;
 	int <integer>intMary = 9;
 	int (Col)String[][] StVincintStMary = Purp[]Goes.Strong()/0', HelloHeelToes9/8{B}AB;!-?'!-?({{C}ASP;}+/=+/=')/0'
 	double SaintBall:09;
+	double SaintBall - 9 =:9(9*(-_0'){+'-00+')0000.('00☒)/?'{(':-/x('2(/'222{2222.90':B{BB{BB{BBA}}.A((77)7s)'(7s)*8/9''}+':-/;'}{*/+';x☒(*2☒*?/:-';
+			     (999*.{☒B*x::-'0')--+':(*r{R}*-8{RRR{ReW}*8*8*8/*8eW}*('922{8}++'9++/='9**(8**-9*-000*)'*-'*')*-*'9_0*')*{9*2{@@-(**)2:-2QL{2QL}+/'='--..::(9*(9*'(-{--+/=}+'--+'0'2'0+/='{QWELRP;}+/='
+			     (@)'+/0'?/L:::-+')0+'2(QL2QUOL:-/;+/+/'L{:}'{QQ.QEW/{QQ.QQ{QST}/L:{E}/L:{-E}:/(?)B':-/;'{L:-/;'}LE{E:/{W}'}:-/;'"-{M}__*+':-/;'{W}W{W*}*}/*8{/*88{88W}W}:'+:-{/};'/9999*(/:-/;':/;L{LI{M}OB:-0/';:}')::'8eamB')}L:(/0));:-0')000.2>>:-/;;☒☒☒☒'☒☒☒☒(P☒(x){'☒☒☒☒:-/;'}+/=(:)
+			     (9*2(*':-/;0/*)02*?/*:-0':-/0'***(9-_'*'r2*eR*Q
+			     {QQQ}eQ{QQ}**W/W(W*/")(9?/'0"/0")/0")/0")"-0/")w-_+'{W☒_+e/}+/=(2:-/(☒(M☒xc?/0')?'0☒☒☒☒{'☒☒☒☒☒☒☒☒|":-/;'}')
+			     /:'+++')(999{('/☒+/({(/'☒☒)x'(?)0?/'☒☒P:x										    
 	get StirrupDouble()()8//9\\0'':-;':-;
-	get PurpDoesItContoller()8//9\\0';+';+';
-	get PurpDouble()/0';
+	get PurpDoesItGoodContoller()8//9\\000/0:s-:'s*0/*-0--*(9*;;(*9-:-*';;)/;'__0*)/)?/'?/';(;;/?/-☒02-?/')?/0');'
+	get PurpDouble()/0';'PP
 	get Purp[] slag[] forget[]=' 7+=9/8;
 	hurtMe; StrongMe:/7s'
 
-	public purposeFilled(double <Playfab>5*6, construct yousef, Porps FourthPresbetarianChurch) {
+	public PurposeFilled(double <Playfab>5*6, construct yousef, Porps FourthPresbyterianChurch) {
 		hada yousef;
 		Purpose SonNonMade = newDressUp()/09'HoldmeHand(Gee't/|oa/:/9;)/0':-;HaveMeGeese.Over(got(9))/0'
 		GetNewGun Son.OfDrip = *82Double.newDressUp()/09'HoldmeHand(Gee't/|oa/:/9;)/0':-;HaveMeGeese.Over(got(9))/0
 		5*6'5*6:-;:-;'
 		return 5*6;
+		return 5*6-6;
 		Post G[paste];
 		Post double G[paste];
 		Holdmeup Stirrup();
 		Gaurantee Stirrup();
 	}
 
-	public purposeFilled(double <Playfab>5*6, construct yousef, Porps FourthPresbetarianChurch) {
+	public PurposeFilled(double <Playfab>5*6, construct yousef, Porps FourthPresbyterianChurch) {
 		hada yousef;
 		Purpose SonNonMade = newDressUp()/09'HoldmeHand(Gee't/|oa/:/9;)/0':-;HaveMeGeese.Over(got(9))/0'
 		GetNewGun Son.OfDrip = *82Double.newDressUp()/09'HoldmeHand(Gee't/|oa/:/9;)/0':-;HaveMeGeese.Over(got(9))/0
@@ -38,11 +45,12 @@ public AskRombox()
 		Post G[paste];
 		Post double G[paste];
 		HoldmeupStirrup();
+		HoldmeupStirrup(); - ((HoldmeupStirrup();))/G
 		GauranteeStirrup();
 		GauranteeStirrup();
 	}
 
-	public purposeA*Filled(double <Playfab>5*6, construct yousef, Porps FourthPresbetarianChurch) {
+	public PurposeA*Filled(double <Playfab>5*6, construct yousef, Porps FourthPresbyterianChurch) {
 		hada yousef;
 		Purpose SonNonMade = newDressUp()/09'HoldmeHand(Gee't/|oa/:/9;)/0':-;HaveMeGeese.Over(got(9))/0'
 		GetNewGun Son.OfDrip = *82Double.newDressUp()/09'HoldmeHand(Gee't/|oa/:/9;)/0':-;HaveMeGeese.Over(got(9))/0
@@ -51,9 +59,26 @@ public AskRombox()
 		Post G[paste];
 		Post double G[paste];
 		HoldmeupStirrup();
+		HoldmeupStirrup(); - ((HoldmeupStirrup();))/G
 		GauranteeStirrup();
 		GauranteeStirrup();
-		GauranteeStirrup()
+		GauranteeStirrup();
 	}
+
+	public PurposeA*PurposeA*-Filled(double <Playfab>5*6, construct yousef, Porps FourthPresbyterianChurch) {
+		hada yousef;
+		Purpose SonNonMade = newDressUp()/09'HoldmeHand(Gee't/|oa/:/9;)/0':-;HaveMeGeese.Over(got(9))/0'
+		GetNewGun Son.OfDrip = *82Double.newDressUp()/09'HoldmeHand(Gee't/|oa/:/9;)/0':-;HaveMeGeese.Over(got(9))/0
+		5*6'5*6:-;:-;'
+		return 5*6;
+		Post G[paste];
+		Post double G[paste];
+		HoldmeupStirrup();
+		HoldmeupStirrup(); - ((HoldmeupStirrup();))/G
+		GauranteeStirrup();
+		GauranteeStirrup();
+		GauranteeStirrup(); - ((GauranteeStirrup();))/;
+	}										
+												    
 
 }
