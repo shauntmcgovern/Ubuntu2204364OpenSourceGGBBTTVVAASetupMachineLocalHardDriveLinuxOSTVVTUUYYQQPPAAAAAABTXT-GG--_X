2b7ec95af8a884fdffe043892204a5535a0443a7
11X(T)🍌🚒🚒🚒🚒🚒X:AAAAAAAAAATATATATATATATATATATATATATATA🚒🚒🚒TATATATATATATATATATATATATATA'TA'|TATAITX'{TT:}[''':;"'{'}['\:"|"'{M}[':]:'[':]:IMX'{CC}['''X:;"'{M}['\:"|"'{M}[':]:I'{' '}['''X:;"'{M}['\:"|"'{M}[':]:'[':]:IꞮT'{PP}['''X:;"'{M}['\:"""|🚒🚒🚒🚒|🦍NT🦹‍♂️ <shauntmcgovern@gmail.com>
2025-05-09 18:38:16 +0000

    PP'
    AA'(here are the 3 classes:Public'repo Github::')submission:09

---
+++ install/ArtificialIntelligence/AskRombox.java
@@ -0,0 +1,121 @@
+public AskRombox()
+{
+	Forclose()9/0;
+	ForNandy()8..9/0'\8.09..?/'/02?/0?/'((00.0[k]')))
+		00.{[<kk.80t?/'>]}+/=M?/'+/=?(??)kkk[v0d]{w}>.?/'
+		     =*PS*80t(//)(9/?'0)\\0')"-_{W/M}'+/='+/='{P}I{'O/'{P}Kimmel,{O}IP=/'M+'=('kkmmL'kkmm{w}L'kkmmL{w}{w}.809/{[(<t>)]}
+			     'tvs/'tv{s}{ssv}/')/'<,M.>;'<,M.>;
+	Found()()*B*A//Tx*//(\\xy[y]*2)*B*A//Tx* Gem[And 2];
+	Found Gem*2;
+	Found Purpose ()2ae'):-;00/')2({2')}0.A9'2A9?/_::'2/000.P')'A{M}IOP;)'{--}7s'+/='--__'Q'7s'7s?/2+/='{P}IOP;
+        Found Purpose ()2ae')::-;(3ae)/09':-;:-;'({P}PIO{Q}ISO::'-;'{S}'(&)'::-;(P);/0'(000/k(k)/k./0[k]00s'::-;'(&&.7[7&k'7&k])(5&[5&k])'&[&k'(&k):':-;'&k'](&87(&87)sksksk,s34,_(&&.807)'::(-');::'!/'0
+
+	double Tom/(//)'Roy = 8990.o.p;
+	double Tom/(///.9-')0/'(3)(3)3f';
+	int <integer>intMary = 9;
+	int (Col)String[][] StVincintStMary = Purp[]Goes.Strong()/0', HelloHeelToes9/8{B}AB;!-?'!-?({{C}ASP;}+/=+/=')/0'
+	double SaintBall:09;:09;:[09;]+/='(777.B/s)+-'(777.B/s/0':-;:-;(6);
+	double SaintBall()() PS*09-[][][.BAT].F//.F//_':':-;:':-;+7(7)!+7(7)!8.;(**)'/(09)(:':-;'**"__':':-;(k).PS09*'F[1] Method[>*PSAT;=='].>>?/'({"-'})(9)9=:99.9
+			     (9*(-_0'){+'-00+')0000.('00☒{QWERL;})/?'{(':-/x('2(/'222{2222.90':B{BB{BB{BBA}}.A((77)7s)'(7s)*8/9''}+':-/;'}{*/+';x☒(*2☒*?/:-';
+			     (999*.{☒B*x::-'0')--+':(*r{R}*-8{RRR{ReW}*8*8*8/*8eW}*('922{8}++'9++/='9**(8**-9*-000*)'*-'*')*-*'9_0*')*{9*2{@@-(**)2:-2QL{2QL}+/'='--..:
+			     :(9*(9*'(-{--+/=}+'--+'0'2'0+/='{QWELRP;}+/='(@)99'+/0'?/L:::-+')0+'2(QL2Q{QQ}UOL:-/;+/+/'L{:}'{QQ.QEW/{QQ.QQ{QST}/L:{E}/L:{-E}:/(?)B':-/;'{L:-/;'}
+			     LE{E:/{W}'}:-/;'"-{M}__*+':-/;'{W}W{W*}*}/*8{/*88{88W}W}:'+:-{/};'/9999*(/:-/;':/;L{LI{M}OB:-0/';:}')::'8eamB')}L:(/0));:-0')000.2>>:-/;;
+			     ☒☒☒☒'☒☒☒☒(P☒(x){'☒☒☒☒:-/;'}+/=(:)(9*2(*':-/;0/*)02*?/*:-0':-/0'***(9-_'*'r2*eR*Q{QQQ}eQ{QQ}**W/W(W*/")(9?/'0"/0")/0")/0")"-0/")w-_+'
+			     {W☒_+e/}+/=(2:-/(☒(M☒xc?/0')?'0☒☒☒☒{'☒☒☒☒☒☒☒☒|":-/;'}')/:'+++')(999{('/☒+/({(/'☒☒)x'(?)0?/'☒☒P:x
+	
+	//get StirrupDouble()()8//9\\0'':-;':-;
+        get StirrupDouble()()8//9\\0'':-;':-;
+	get StirrupDouble()()(88.9/0/0)?/'(9//'?'0?'\\"-_/:-;?':-'000.M//')0//?"-_M[MNA0ST]+/=9.09.09B')')
+		(9BOS:-*'?/'//k'):-/;0K*VOS'*9'(*::-;'*0*):-;?/':-:-/0;-/(-_/'{L}+/M='(9-'0'L:-/;"-_M'{K}{L}
+			I*')I*-O*-P={+/[==']='}+/K*MAST;/;SUPPOST*TAM/*TABLE(*09)'PurpMostStruggleD*9()()';
+	
+	get PurpDoesItGoodContoller.B(&G8T.k)(888)888(kkk)/:-;'/0'(88.00/':-;(&)
+				    -'(88\99/00)+-':-;:-;'(&)(&7&8&9&(s)s&$')/
+		                    09::'-;'_--(77.7s)+'(+':-;)8.9(99.9)//0'//'(ft)9.(ft)\
+			            (\.k)\((:)//(./?k:-/0k;)/09B'000/0(0B(B)):s(ss)&8(k)/0::
+			            '-;(8)::'-;(9k)[k]-[k]{K}M{[(M)]}'s/k(PP)'::-;{I}OS'::-;T{TT}-:'
+			            (:/s)::'(78[5k]'::(-(8)k'::-;(&&'k)(&&'k.(:(&k)8&k'::'8&k':?/0&8'kk&8
+				    (-:'(88.::-;'/9::-;(k){N}IOP'::-;(&)'::-;'-+(8&(8&k)'k(kk))'(8&k)8&.&k(&k.9):k/')'::-;+7s/
+				    088.7(8)9[(k)][(k)]'*-('::-;+/7s/7s)[7s][7s]++/'-::7s'
+				    (:)'s(ss0$8)/*8/-+'(::-;'(::-')(__8--9[kkkv809.99/(99)/"s'
+				    ::-;('::-(&);-:'__++'7''!':-;:-;(&&708')/8/9//0'kklvm':-;(&-')++/0+st]'--'(***/'):-;'
+				    /(**)/8*{[(kk.v::'-/m)]}'::-;'(&-/+(8/9)::-;'('::-;(P/K)((+))
+				    *0/*-0--*(77.9(*809)*/"-'*/"-'*/"-'*;(77);((809)*9(80$.9k)
+				    +-:-;'_&(&&)'::-('(&):-;(::'-;(k)::-'(')(k);-+('::(:)'-+(77)
+				    (77-88-0(k)-++!(7k)/'::-;)/00.0?/x?/x(xx)'-:-(::-;')(:-'(PPN{N}::I'::-;'O'::-;
+				    (8)9(&::-;'{[(9k)]}::'-:-*';;)/;'__0*)/)?/'?/';(;;/?/-☒02.02-?/')?/0');';/;(k)
+								     
+	get PurpDouble()/0';'PP
+	get Purp[] slag[] forget[]=' 7+=9/8;
+	get Purp[][] Quotient().89/.-':-;':-;(&)/0'Then.NextOne();
+	hurtMe; StrongMe:/7s'
+	Purple()() Negro::-(+)/0':-;':-;'/00(00/)':-;:-;(&)/0.889(77).9
+	Purple.next()()8.9(.99)/0-++{+}+-'(&):-;_--'++=/M({<.'::-{m}\§'
+	Purple.next()()/8.9(.99.7+(-':-;{:-;}'°
+	Purple.next()()-+'8/9/0'(&)7.8.9(8.9)/0'
+
+        Purple.next();
+
+	A*
+	public PurposeFilled(double.f <Playfab.f>5*6(5*6), <C>Construct yousef, Porps FourthPresbyterianChurch) {
+		hada yousef(2);
+		Purpose SonNonMade = newDressUp()/09'HoldmeHand(Gee't/|oa/:/9;)/0':-;HaveMeGeese.Over(got(9))/0'
+		GetNewGun Son.OfDrip = *82Double.newDressUp()/09'HoldmeHand(Gee't/|oa/:/9;)/0':-;HaveMeGeese.Over(got(9))/0
+		5*6'5*6:-;:-;'
+		return 5*6(5*6);
+		return 5*6-6;
+		Post G[paste];
+		Post double G[paste];
+		HoldmeupStirrup();
+		GauranteeStirrup().2;
+	}-++':-;(8)-A(AA)
+				   
+        B*B
+	public PurposeFilled(double.f <Playfab.f>5*6(5*6), <C>Construct yousef, Porps FourthPresbyterianChurch) {
+		hada yousef;
+		Purpose SonNonMade = newDressUp()/09'HoldmeHand(Gee't/|oa/:/9;)/0':-;HaveMeGeese.Over(got(9))/0'
+		GetNewGun Son.OfDrip = *82Double.newDressUp()/09'HoldmeHand(Gee't/|oa/:/9;)/0':-;HaveMeGeese.Over(got(9))/0
+		5*6'5*6:-;:-;'
+		return 5*6;
+		Post G[paste];
+		Post double G[paste];
+		HoldmeupStirrup();
+		HoldmeupStirrup(); - ((HoldmeupStirrup();))/G
+		GauranteeStirrup();
+		GauranteeStirrup();
+	}-:-;'(:-;')-B
+			    
+        C*C*(C-*)
+	public PurposeA*Filled(double.f <Playfab.f>5*6(5*6), <C>Construct yousef, Porps FourthPresbyterianChurch) {
+		hada yousef;
+		Purpose SonNonMade = newDressUp()/09'HoldmeHand(Gee't/|oa/:/9;)/0':-;HaveMeGeese.Over(got(9))/0'
+		GetNewGun Son.OfDrip = *82Double.newDressUp()/09'HoldmeHand(Gee't/|oa/:/9;)/0':-;HaveMeGeese.Over(got(9))/0
+		5*6'5*6:-;:-;'
+		return 5*6;
+		Post G[paste];
+		Post double G[paste];
+		HoldmeupStirrup();
+		HoldmeupStirrup(); - ((HoldmeupStirrup();))/G
+		GauranteeStirrup();
+		GauranteeStirrup();
+		GauranteeStirrup();
+	}f':-;f':-;(f':-;)-C*CC-C;
+
+	D*
+	public PurposeA*PurposeA*-Filled(double.f <Playfab.f>5*6(5*6), <C>Construct yousef, Porps FourthPresbyterianChurch) {
+		hada yousef;
+		Purpose SonNonMade = newDressUp()/09'HoldmeHand(Gee't/|oa/:/9;)/0':-;HaveMeGeese.Over(got(9))/0'
+		GetNewGun Son.OfDrip = *82Double.newDressUp()/09'HoldmeHand(Gee't/|oa/:/9;)/0':-;HaveMeGeese.Over(got(9))/0
+		5*6'5*6:-;:-;'
+		return 5*6;
+		Post G[paste];
+		Post double G[paste];
+		HoldmeupStirrup();
+		HoldmeupStirrup(); - ((HoldmeupStirrup();))/G
+		GauranteeStirrup();
+		GauranteeStirrup();
+		GauranteeStirrup(); - ((GauranteeStirrup();))/;
+	}':-;':-;(&)(&)/7sz(*)$'D*-D										
+
+        HoldMyHand();
+}*BST::(:)-;'(PST:-;')/9'
---
+++ install/ArtificialIntelligence/VersusAnnonymousIndex.h
@@ -0,0 +1,107 @@
+public class VersusAnnonymousIndex
+{
+	conuDrum Graph Index(); = new startOfIndex(Array[] startIndex[];)P'
+	WhatYouDontHave and WhatYouDoHave(); = PHP()Index(Y:-'0'):-;0/')'Y'::-0');?/'PurpDoesHave(g);0')PurpDoesHave(G'Y?/0'D')::-0/;"-':-'{L}"-'?/
+        Graph DontHave() = new Index();, Purpose NewIndex(); xsg(cy)*809_('9_0')y'*8(9a(*8_9('Y**-'r'--(%):-/;L:')plurpdo*sga*tg{(9_0')==--{++/='}[-PIO{P}]'|\k'
+        HoldOn = NewIndex;
+        Purpose HoldOn = NewIndex(*98st(9)0;'g*(9*--0')y*'(9--0**')startIndex'
+
+	HaveGram *(9*--(*-0)*)str*y*r-0L')st*ry = new Story();
+	new Story =*98(*start);
+	HaveCloud = *98(Story);'d\%{D}
+	WhatCouldYouHave = *98(Story) *\'(%%)'(%☒)0%☒':0%7(%?/')9/%0'/7%;
+	WhatCouldYouHave *' ((WhatCouldYouHave)9(*9\\'0*)*B' **(9*--0*')''::-;'Y::-☒;'{Y}'IOM☒P;'B
+	WhatCouldYouNeed = newPlurpStory(*98st*9('rstgy; 0;')z78*9('*8__0*')*BST(90csgyz(0)(0☒)z:-;L'   ('\\BY'I'O(☒)'{'U'☒/?/☒'{{U}I☒OMP;}'Y'U☒IOM/P?/''y'YUIO☒MP(P☒')(P☒);/+/='
+	WhatIsItYouHoldDear'*(0x☒☒'(9☒-'☒'Y'x'☒;'YGu/x;(xyu☒gz(y)'9*8(st'st*8('9y/z(gcoZ☒☒*Y'(cyGo//'{☒☒:☒☒;Y"-'?/'<,M.>?/'?/'[][];
+	WhatIsItYouHoldDearAndCrap;'98*9(*st*(3.9?/'(startNewGutIndex();):-;'
+
+	conuDrum Graphs new Graphs()PSg/x;   =/☒;?/'"-_k'
+	conuDrum new PointIndex()'PSg(9)/x☒;x☒;🛡️🛡️/?;(G):-/;G//0')/;L:'((🛡️🛡️:-(G)0')?/'((GG/:-(☒)0:-/;L:"<,M.>GG:-;'(%)M?/'//(9//'0')Y//GG/9x;(?)0?/x'
+	WhenYoulHoldThatTimeWhatComes = (p)Index;(gc((0//?/(0)?/'/(0?|'))/0?/;'/'(p)'Index;(gc'0/'g'c/g☒;'(9?/☒x;'0.>M{(<,x(C)cvo☒;..::-;'L:')};'/?':-;L'P
+	WhenYouHoldItWhenThatTimeComes; WhenYouGotItWhenThatTimeComes(s(C)c'0)'G//?'(Yc'9'-0'0?/cxy(9?:-☒0';0')?/':-;L' 
+	BA{BB}'AST☒?/'x::-0:')x☒:-C'0'9?/☒'☒☒:(c'//?9:--;:'-0:-/;'☒cx9::-')00☒☒?/'gg'(c'2(9c07cgyz'::-:''(G(c🛡️)9🛡️🛡️'-0')c'Y'cgz☒ozc'Y'c'z'{cGsY'-}__+/='7🛡️🛡️(🛡️🛡️)'?/'-;'__+/='7?/'-;'
+        WhatYouWouldHaveThenWhatItComesNext(cs09csgYZ9);'Hold(g)my{g}Hand{(90csyGyu-I-O-'P'-csyGyu-{Gi}-{Gi}-'P
+		::'9-0)'Y'/B?'BB--csyGyu-{Gi}-{Gi}-'P::'9-0)'Y'/B?'{BB}'A;(csyg☒(cs☒g(9☒☒'-☒'0'Y☒☒'-U-I-0'9'{Y☒☒}'-|/-'I'-O'}☒?/'"-P('☒☒")_'"-P(☒?/x"-'_'{Q}_
+		'x'9☒?/x☒:-;'(g_(x)☒9*☒_x(c*y☒_0*)/_::-;'☒☒:-;'9?/x(xxy☒:i:x;98☒0:-w:-☒0☒')9VV8-'P*x'-_☒☒:-9--**(&2^s>.?/x'
+		WhenItHappendNext(g_(x)☒9*☒_x(c*y☒_0*); And(When)It('9*7*7:00:00'☒☒:9--☒0☒0)'x'y'--'☒☒'-T'--Y--u(☒-)(☒-)(☒-)yui-O☒-xlp;?/'?/'):-;?/'L:' conflagastplyf☒:-;'☒:-;'{(L);09'-0)')rsty☒_r_L:'?/'?/'?/'<,M.><,M☒.><,M☒.>"
+		HolditOnToItHappenedNext/mc:"?/'L:'   HoldOntoi-{M☒c(//)0:-;/{G}'G{(//co?/'☒☒//F'C{H}/:{I'}OP;/;'PPio{g}'--0')M☒c(//)0:-;/{G}'G{(//co?/'☒☒//F'C{H}/:{I'}OP;/;'PPio{g}'--0')M☒c(//)0:-;/{G}'G{(//co?/'☒☒//F'C{H}/:{I'}OP;/;'PPio{g}'hadHappened(g);
+		MeasuredOnTough(g);
+		FullOnToNextBrow(ag);
+	WhatYouWouldHaveThenWhatItComesNext(cs09csgYZ9);'Hold(g)my{g}Hand{(90csyGyu-I-O-'P'-csyGyu-Purpose FullOnIndex();g/(G)x{//u}'t{T☒☒☒Y'2//{(y-'t'x}{('I'
+	WhatYouWouldHaveThenWhatItComesNext(cs09csgYZ9);'Hold(g)my{g}HandGun(9s☒cxyz);
+	HadOnANote(99(s☒0')☒☒G'{(T'}x's:y:'Y':s:'I'-O(xy)')-'P(xy)';
+
+	HadATimeNote();
+	FurtherLessLookGlass()8-9;
+	FutherLessLookGlass() - 9(9)HadATimeNote(B\X);
+	Burlap(c(\\x')c☒ox\'=2;=2;
+
+	public class Burlap(c(\\x')c☒ox\'=2;=2;()()c\☒; d/x{
+	P{<N>}c☒\x'c//(☒cx}'t)tt'xc☒☒{☒☒}'jx\xo'☒☒{tt}'1/x☒'☒☒'t:co
+		Apple() dx/y'= cd((cd/☒☒'))/a☒m;
+		Purpose()OnFilled' = cxt'☒☒/☒☒(m/mx'tco//m/xm/{/xm\}/x'(t'(c//x'tco☒m☒☒'ct{ctx☒}xco//'xctct'x☒m//0x☒{'c☒{ctx☒mmc{mmc}t"ox//m/☒?/;m"}xt{xt☒}'cho"m.999.'(ctO"-0.ctO"/")"-_/MMMM{_N}M?'
+		aDeHem()9/☒☒☒/☒☒'ct90.P/x;
+		aDeHem()9/☒☒☒/☒☒'ct90.P/x; // aDeHem()8/☒☒☒/☒☒'ct80.P/x;
+		aDeHem()9/☒☒☒/☒☒'ct90.P/x; // aDeHem()8/☒☒☒/☒☒'ct80.P/x; // aDeHem()(((8)))/☒☒☒/☒☒'ct(((8)))0.P/x;
+		aDeHem()9/☒☒☒/☒☒'ct90.P/x; // aDeHem()8/☒☒☒/☒☒'ct80.P/x; // aDeHem()(((8)))/☒☒☒/☒☒'ct(((8)))0.P/x; // aDeHem()(((8)))/☒☒☒/☒☒'ct(((8)))0.P/x;//('fixturing and transposing)
+		aDeHem()9/☒☒☒/☒☒'ct90.P/x; // aDeHem()8/☒☒☒/☒☒'ct80.P/x; // aDeHem()(((8)))/☒☒☒/☒☒'ct(((8)))0.P/x; // aDeHem()(((8)))/☒☒☒/☒☒'ct(((8)))0.P/x; // aDeHem()((((8))))/☒☒☒/☒☒'ct((((8))))0.P/x;'P//'(handling and confixing'PST09):-;
+		P/x;
+	}
+	public class aDeHem/9()()()/x'tc((ct'x//co{ct'x/m?t'}'//t{ctO'//c☒O':tM\
+				  (☒?cOt?\M""-:-;?/'{L}{LMN/☒T;}QQ{QQ}\)Q{QQ}cO.tm?/'mc{ccT}/T"\x'{T}:
+				  c/o:-0m"?/m'<,{M{MM/☒'T"-_/'nC{.nC{<,nC.>}}<,M:.<,.>{>M/☒'}.>MM
+				  {/c☒x:Tco"-x}M':-;:-;'M?/'<,xM..P☒-N?'-X'{X}'I+0)+-'"-_{Mm/m
+			          {<,{<,(M).>}|"NIO{<,(P).>};:-/;(??)9}'?.__0}""-/{M}?/'?:'"2{2CMM/?'{MM}}C?
+				  /'☒☒/☒☒c:(8"/2"c"?/'?/'}M/cMx☒c/T.C"/?'m<,M{N}.>?/'P{{P}IO/P{PP};}"|
+				  ':-;':-_L/;'>..>""}':;'((cc:;'))
+
+	public class '*/_({'__'}0:/;?/'/?:'?/:'((aDeHem/9))*8(*808)()()/x'tc((ct'x//co{ct'x/m?t'}'//t{ctO'//c☒O':tM\
+				  (☒?cOt?\M""-:-;?/'{L}{LMN/☒T;}QQ{QQ}\)Q{QQ}cO.tm?/'mc{ccT}/T"\x'{T}:
+				  c/o:-0m"?/m'<,{M{MM/☒'T"-_/'nC{.nC{<,nC.>}}<,M:.<,.>{>M/☒'}.>MM
+				  {/c☒x:Tco"-x}M':-;:-;'M?/'<,xM..P☒-N?'-X'{X}'I+0)+-'"-_{Mm/m
+			          {<,{<,(M).>}|"NIO{<,(P).>};:-/;(??)9}'?.__0}""-/{M}?/'?:'"2{2CMM/?'{MM}}C?
+				  /'☒☒/☒☒c:(8"/2"c"?/'?/'}M/cMx☒c/T.C"/?'m<,M{N}.>?/'P{{P}IO/P{PP};}"|
+				  ':-;':-_L/;'>..>""}':;'((cc:;'))
+
+	
+	public class aDeHem {'/0)}"-_/m'"-_/M{+M}'{P}IOP;PIOP; {(/0?'Conflabtureguss"{-"-_?/(??)"-_{w}++/==
+								'{T'}I:/?/'{0')}{-TI"-0"){-O_'[]'-_:-/;}'+P}}
+								+/='[/='][/==']HadItAll;
+	
+	public class aDeHem {'/0)}"-_/m'"-_/M{+M}'{P}IOP;PIOP; {(/0?'Conflabtureguss"{-"-_?/(??)"-_{w}++/==
+								'{T'}I:/?/'{0')}{-TI"-0"){-O_'[]'-_:-/;}'+P}}
+								+/='[/='][/==']HadItAll;HadItAll;
+
+	
+	public GrossWork(""92*&/E_W*':-;:-;:-;""9('-2?/"0/"-?/2?0/"-0')-PENS-}'IVE;7s?/v;7s?(PP)/v;
+	public GrossWork(""92*&/E_W*':-;:-;:-;""9('-2?/"0/"-?/2?0/"-0')-PENS-}'IVE;7s?/v;7s?(PP)/v;((public GrossWork(""92*&/E_W*':-;:-;:-;""9('-2?/"0/"-?/2?0/"-0')-PENS-}'IVE;7s?/v;7s?(PP)/v;)) - public GrossWork(""92*&/E_W*':-;:-;:-;""9('-2?/"0/"-?/2?0/"-0')-PENS-}'IVE;7s?/v;7s?(PP)/v;v;v;
+	
+	aDeHem;
+																										      
+	aDeHem()':-;();!/?
+	FolsomPrisonBlues();
+        FolsomPrisonBlues.2;
+        NewHandHoldEmKnowHowToShakeThem()-+':-;(-+/':-;(':-;);
+	ForgetMeNots();
+        PurposeSoGlone()-+-+-+'(88)'/2.//9/0/90';*PP/(*PP)
+        PurposoSoGlone.Xanax();
+        PurposeSoGlone.Diaphragm();
+	PurposeSoGlone.DiaphragmNextTwo();
+	SoGlone':-;':-;(:-;(P):-;/+'(++/☒++-(0.9.8)/9/9.+'07.8.9.00//0(<{<.-':>ft-i'::-;}>:0):0/)'
+	SoGlonePurpose();ft'
+
+        PurposeSoGlone()'::(:):-;(f)&'&:-;'&(&&.77.88/.99&:'.(8.9.000.{00.&B}.>/':-;:-;/99.00(9/)
+	                ':-;(:-;')&/09988.7&(7)s'"_'"__('m.007&780).0(fd&fg)/0::-;'"{M}Das'::(&)-;
+			':-;+'_/'?(&??.__'/$__:'-++/09/009988(88/st'::-;{B}.{B}.>='{&}A&.S'::-;
+			(':-;).T"_':-;""(")':-;&';
+
+        HadEnoughinmyhand();
+        PluralTwoTimes'::-;':(:)-;(f)&45.6-+'_&';
+	Xpergenesis();8.9/0':-;(f)
+	Dual'::-;()8.9.0/('::-;_f(&'::-;/0'((g'::-;))/'::-;Handed'::-;((g-+/))'::-;Plot;				 
+	Hadagood((':-;(('::-;/00(_::'(&)ft_'::(:)-;(&&.-.6(66)-7.+.88//☒+\\.0☒99.+(88).7.☒8☒.☒//☒0☒t'::-;				 
+
+        Hadagood();
+
+					 
+}*PST09;
