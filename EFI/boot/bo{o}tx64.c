5cdf5421e37b8f862194542bbedf86670bdea079
Shaun McGovern <shauntmcgovern@gmail.com>
2026-07-22 14:36:10 -0500

    'The EFI files
    'P{S}T::-')';

---
+++ EFI/boot/bo{o}tx64.c
@@ -0,0 +1,212 @@
+File Type: WINDOWS EXECUTABLE
+
+File Header:
+  Machine:                       0x8664 (AMD64)
+  Number of Sections:            10
+  Time Date Stamp:               0x00000000
+  Pointer to Symbol Table:       0x000CAE00
+  Number of Symbols:             3678
+  Size of Optional Header:       0x00F0 (240)
+  Characteristics:               0x0206
+    File is executable
+    Line numbers stripped from file
+    Debugging info stripped from file in .DBG file
+
+Optional Header:
+  Magic:                         0x020B
+  Linker Version:                2.41
+  Size of Code:                  0x00060800 (395264)
+  Size of Initialized Data:      0x0006A200 (434688)
+  Size of Uninitialized Data:    0x00000000 (0)
+  Address of Entry Point:        0x00023000
+  Base of Code:                  0x00023000
+  Image Base:                    0x0000000000000000
+  Section Align:                 0x00001000
+  File Align:                    0x00000200
+  Operating System Version:      0.0
+  Image Version:                 0.0
+  Subsystem Version:             0.0
+  Size of Image:                 0x000D7000 (880640)
+  Size of Headers:               0x00000400 (1024)
+  Checksum:                      0x000FBF71
+  Subsystem:                     0x000A (EFI Application)
+  DLL Characteristics:           0x0000
+  Size of Stack Reserve:         0x0000000000000000 (0)
+  Size of Stack Commit:          0x0000000000000000 (0)
+  Size of Heap Reserve:          0x0000000000000000 (0)
+  Size of Heap Commit:           0x0000000000000000 (0)
+  Loader Flags:                  0x00000000
+  Number of RVA and Sizes:       16
+
+Data Directory:
+  Export directory:               VA: 0           Size: 0
+  Import directory:               VA: 0           Size: 0
+  Resource directory:             VA: 0           Size: 0
+  Exception directory:            VA: 0           Size: 0
+  Security directory:             VA: 0x000E9A78  Size: 0x00002590 (9616)
+  Base relocation table:          VA: 0x00084000  Size: 0x0000000A (10)
+  Debug directory:                VA: 0           Size: 0
+  Architecture-specific data:     VA: 0           Size: 0
+  RVA of global pointer:          VA: 0           Size: 0
+  Thread local storage directory: VA: 0           Size: 0
+  Load configuration directory:   VA: 0           Size: 0
+  Bound import directory:         VA: 0           Size: 0
+  Import address table:           VA: 0           Size: 0
+  Delay load import descriptors:  VA: 0           Size: 0
+  COM run-time descriptor:        VA: 0           Size: 0
+  (unknown directory entry):      VA: 0           Size: 0
+
+Section Table:
+  Section Header #1
+    Name:                             /4
+    Virtual Size:                     0x0001DB3C (121660)
+    Virtual Address:                  0x00005000
+    Size of Raw Data:                 0x0001DC00 (121856)
+    File Pointer to Raw Data:         0x00000400
+    File Pointer to Relocation Table: 0x00000000
+    File Pointer to Line Numbers:     0x00000000
+    Number of Relocations:            0
+    Number of Line Numbers:           0
+    Characteristics:                  0x40000040
+      Section contains initialized data
+      Section is readable
+
+  Section Header #2
+    Name:                             .text
+    Virtual Size:                     0x000606BD (394941)
+    Virtual Address:                  0x00023000
+    Size of Raw Data:                 0x00060800 (395264)
+    File Pointer to Raw Data:         0x0001E000
+    File Pointer to Relocation Table: 0x00000000
+    File Pointer to Line Numbers:     0x00000000
+    Number of Relocations:            0
+    Number of Line Numbers:           0
+    Characteristics:                  0x60000020
+      Section contains code
+      Section is executable
+      Section is readable
+
+  Section Header #3
+    Name:                             .reloc
+    Virtual Size:                     0x0000000A (10)
+    Virtual Address:                  0x00084000
+    Size of Raw Data:                 0x00000200 (512)
+    File Pointer to Raw Data:         0x0007E800
+    File Pointer to Relocation Table: 0x00000000
+    File Pointer to Line Numbers:     0x00000000
+    Number of Relocations:            0
+    Number of Line Numbers:           0
+    Characteristics:                  0x42000040
+      Section contains initialized data
+      Section can be discarded
+      Section is readable
+
+  Section Header #4
+    Name:                             /14
+    Virtual Size:                     0x0000006B (107)
+    Virtual Address:                  0x00086000
+    Size of Raw Data:                 0x00000200 (512)
+    File Pointer to Raw Data:         0x0007EA00
+    File Pointer to Relocation Table: 0x00000000
+    File Pointer to Line Numbers:     0x00000000
+    Number of Relocations:            0
+    Number of Line Numbers:           0
+    Characteristics:                  0xC0000040
+      Section contains initialized data
+      Section is readable
+      Section is writeable
+
+  Section Header #5
+    Name:                             /26
+    Virtual Size:                     0x0000006A (106)
+    Virtual Address:                  0x00087000
+    Size of Raw Data:                 0x00000200 (512)
+    File Pointer to Raw Data:         0x0007EC00
+    File Pointer to Relocation Table: 0x00000000
+    File Pointer to Line Numbers:     0x00000000
+    Number of Relocations:            0
+    Number of Line Numbers:           0
+    Characteristics:                  0x40000040
+      Section contains initialized data
+      Section is readable
+
+  Section Header #6
+    Name:                             .data
+    Virtual Size:                     0x0002DD34 (187700)
+    Virtual Address:                  0x00088000
+    Size of Raw Data:                 0x0002DE00 (187904)
+    File Pointer to Raw Data:         0x0007EE00
+    File Pointer to Relocation Table: 0x00000000
+    File Pointer to Line Numbers:     0x00000000
+    Number of Relocations:            0
+    Number of Line Numbers:           0
+    Characteristics:                  0xC0000040
+      Section contains initialized data
+      Section is readable
+      Section is writeable
+
+  Section Header #7
+    Name:                             /37
+    Virtual Size:                     0x00002721 (10017)
+    Virtual Address:                  0x000B6000
+    Size of Raw Data:                 0x00002800 (10240)
+    File Pointer to Raw Data:         0x000ACC00
+    File Pointer to Relocation Table: 0x00000000
+    File Pointer to Line Numbers:     0x00000000
+    Number of Relocations:            0
+    Number of Line Numbers:           0
+    Characteristics:                  0x40000040
+      Section contains initialized data
+      Section is readable
+
+  Section Header #8
+    Name:                             .dynamic
+    Virtual Size:                     0x00000100 (256)
+    Virtual Address:                  0x000B9000
+    Size of Raw Data:                 0x00000200 (512)
+    File Pointer to Raw Data:         0x000AF400
+    File Pointer to Relocation Table: 0x00000000
+    File Pointer to Line Numbers:     0x00000000
+    Number of Relocations:            0
+    Number of Line Numbers:           0
+    Characteristics:                  0xC0000040
+      Section contains initialized data
+      Section is readable
+      Section is writeable
+
+  Section Header #9
+    Name:                             .rela
+    Virtual Size:                     0x0001B438 (111672)
+    Virtual Address:                  0x000BA000
+    Size of Raw Data:                 0x0001B600 (112128)
+    File Pointer to Raw Data:         0x000AF600
+    File Pointer to Relocation Table: 0x00000000
+    File Pointer to Line Numbers:     0x00000000
+    Number of Relocations:            0
+    Number of Line Numbers:           0
+    Characteristics:                  0x40000040
+      Section contains initialized data
+      Section is readable
+
+  Section Header #10
+    Name:                             .sbat
+    Virtual Size:                     0x000000C3 (195)
+    Virtual Address:                  0x000D6000
+    Size of Raw Data:                 0x00000200 (512)
+    File Pointer to Raw Data:         0x000CAC00
+    File Pointer to Relocation Table: 0x00000000
+    File Pointer to Line Numbers:     0x00000000
+    Number of Relocations:            0
+    Number of Line Numbers:           0
+    Characteristics:                  0x40000040
+      Section contains initialized data
+      Section is readable
+
+/*
+PE32/PE32+ packaged{Either C(binary{root} style), or {C++}these are large files you need to split them for 100mb a 'piece (only the Pool and Casper files:09)
+editing a working system is out of my developer reach at this time, you can its {open source} perhaps a more custom Linux experience off of the 'kernel' and included with the working terminal which on 'Linux' is direct machine to user relationship and that is how you can for example upload to github if you want.
+('The Linux Kernel'):09;he Linux kernel is the foundational core of the Linux operating system, acting as the ultimate intermediary between your computer's hardware and software. It handles critical system tasks such as memory management, process scheduling, device drivers, and security, allowing applications to interact with hardware without needing specific details about the physical components(Sounds like the very mapping of system execution and application use)
+{IDE}'s
+GNU-EFI or TianoCore EDK II and also, R (programming language), along with the programming languages known as Assembly or Rust
+The EFI System Partition (ESP) and executed by your motherboard's UEFI firmware(to have the operating system working) before the operating system'OS boots
+*/
---
+++ EFI/boot/gr{u}bx64.c
@@ -0,0 +1,142 @@
+File Type: WINDOWS EXECUTABLE
+
+File Header:
+  Machine:                       0x8664 (AMD64)
+  Number of Sections:            5
+  Time Date Stamp:               0x54A48E00 (12/31/2014 7:00:00 PM)
+  Pointer to Symbol Table:       0x00000000
+  Number of Symbols:             0
+  Size of Optional Header:       0x00F0 (240)
+  Characteristics:               0x020E
+    File is executable
+    Line numbers stripped from file
+    Local symbols stripped from file
+    Debugging info stripped from file in .DBG file
+
+Optional Header:
+  Magic:                         0x020B
+  Linker Version:                0.0
+  Size of Code:                  0x0000C000 (49152)
+  Size of Initialized Data:      0x0021D000 (2215936)
+  Size of Uninitialized Data:    0x00000000 (0)
+  Address of Entry Point:        0x00001000
+  Base of Code:                  0x00001000
+  Image Base:                    0x0000000000000000
+  Section Align:                 0x00001000
+  File Align:                    0x00001000
+  Operating System Version:      0.0
+  Image Version:                 0.0
+  Subsystem Version:             0.0
+  Size of Image:                 0x0022B000 (2273280)
+  Size of Headers:               0x00001000 (4096)
+  Checksum:                      0x0022C8C4
+  Subsystem:                     0x000A (EFI Application)
+  DLL Characteristics:           0x0000
+  Size of Stack Reserve:         0x0000000000010000 (65536)
+  Size of Stack Commit:          0x0000000000010000 (65536)
+  Size of Heap Reserve:          0x0000000000010000 (65536)
+  Size of Heap Commit:           0x0000000000010000 (65536)
+  Loader Flags:                  0x00000000
+  Number of RVA and Sizes:       16
+
+Data Directory:
+  Export directory:               VA: 0           Size: 0
+  Import directory:               VA: 0           Size: 0
+  Resource directory:             VA: 0           Size: 0
+  Exception directory:            VA: 0           Size: 0
+  Security directory:             VA: 0x0022B000  Size: 0x00000788 (1928)
+  Base relocation table:          VA: 0x0022A000  Size: 0x00001000 (4096)
+  Debug directory:                VA: 0           Size: 0
+  Architecture-specific data:     VA: 0           Size: 0
+  RVA of global pointer:          VA: 0           Size: 0
+  Thread local storage directory: VA: 0           Size: 0
+  Load configuration directory:   VA: 0           Size: 0
+  Bound import directory:         VA: 0           Size: 0
+  Import address table:           VA: 0           Size: 0
+  Delay load import descriptors:  VA: 0           Size: 0
+  COM run-time descriptor:        VA: 0           Size: 0
+  (unknown directory entry):      VA: 0           Size: 0
+
+Section Table:
+  Section Header #1
+    Name:                             .text
+    Virtual Size:                     0x0000C000 (49152)
+    Virtual Address:                  0x00001000
+    Size of Raw Data:                 0x0000C000 (49152)
+    File Pointer to Raw Data:         0x00001000
+    File Pointer to Relocation Table: 0x00000000
+    File Pointer to Line Numbers:     0x00000000
+    Number of Relocations:            0
+    Number of Line Numbers:           0
+    Characteristics:                  0x60000020
+      Section contains code
+      Section is executable
+      Section is readable
+
+  Section Header #2
+    Name:                             .data
+    Virtual Size:                     0x00010000 (65536)
+    Virtual Address:                  0x0000D000
+    Size of Raw Data:                 0x00010000 (65536)
+    File Pointer to Raw Data:         0x0000D000
+    File Pointer to Relocation Table: 0x00000000
+    File Pointer to Line Numbers:     0x00000000
+    Number of Relocations:            0
+    Number of Line Numbers:           0
+    Characteristics:                  0xC0000040
+      Section contains initialized data
+      Section is readable
+      Section is writeable
+
+  Section Header #3
+    Name:                             mods
+    Virtual Size:                     0x0020C000 (2146304)
+    Virtual Address:                  0x0001D000
+    Size of Raw Data:                 0x0020C000 (2146304)
+    File Pointer to Raw Data:         0x0001D000
+    File Pointer to Relocation Table: 0x00000000
+    File Pointer to Line Numbers:     0x00000000
+    Number of Relocations:            0
+    Number of Line Numbers:           0
+    Characteristics:                  0xC0000040
+      Section contains initialized data
+      Section is readable
+      Section is writeable
+
+  Section Header #4
+    Name:                             .sbat
+    Virtual Size:                     0x00001000 (4096)
+    Virtual Address:                  0x00229000
+    Size of Raw Data:                 0x00001000 (4096)
+    File Pointer to Raw Data:         0x00229000
+    File Pointer to Relocation Table: 0x00000000
+    File Pointer to Line Numbers:     0x00000000
+    Number of Relocations:            0
+    Number of Line Numbers:           0
+    Characteristics:                  0x40000040
+      Section contains initialized data
+      Section is readable
+
+  Section Header #5
+    Name:                             .reloc
+    Virtual Size:                     0x00001000 (4096)
+    Virtual Address:                  0x0022A000
+    Size of Raw Data:                 0x00001000 (4096)
+    File Pointer to Raw Data:         0x0022A000
+    File Pointer to Relocation Table: 0x00000000
+    File Pointer to Line Numbers:     0x00000000
+    Number of Relocations:            0
+    Number of Line Numbers:           0
+    Characteristics:                  0x42000040
+      Section contains initialized data
+      Section can be discarded
+      Section is readable
+
+/*
+PE32/PE32+ packaged{Either C(binary{root} style), or {C++}these are large files you need to split them for 100mb a 'piece (only the Pool and Casper files:09)
+editing a working system is out of my developer reach at this time, you can its {open source} perhaps a more custom Linux experience off of the 'kernel' and included with the working terminal which on 'Linux' is direct machine to user relationship and that is how you can for example upload to github if you want.
+('The Linux Kernel'):09;he Linux kernel is the foundational core of the Linux operating system, acting as the ultimate intermediary between your computer's hardware and software. It handles critical system tasks such as memory management, process scheduling, device drivers, and security, allowing applications to interact with hardware without needing specific details about the physical components(Sounds like the very mapping of system execution and application use)
+{IDE}'s
+GNU-EFI or TianoCore EDK II and also, R (programming language), along with the programming languages known as Assembly or Rust
+The EFI System Partition (ESP) and executed by your motherboard's UEFI firmware(to have the operating system working) before the operating system'OS boots
+*/
